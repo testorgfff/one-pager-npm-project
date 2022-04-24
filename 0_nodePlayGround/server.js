be126679f32cc2921877e8296a5035175175ecf2
Ahmed Yesuf <ahmed.yesuf@faro.com>
2022-04-24 17:33:43 +0200

    Routing examples

--- 0_nodePlayGround/server.js
+++ 0_nodePlayGround/server.js
@@ -5,4 +5,9 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 const port = 8000;
+// app.listen(port, () => {  console.log('We are live on ' + port);});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
 app.listen(port, () => {  console.log('We are live on ' + port);});
